e1098e049c8abc96e9e36ebe89c5945ee31369c4
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-14 18:24:54 +0200

    bump

--- BookstoreApp/Platforms/Android/MainActivity.cs
+++ BookstoreApp/Platforms/Android/MainActivity.cs
@@ -2,7 +2,7 @@
 using Android.Content.PM;
 using Android.OS;
 
-namespace BookstoreApp.Platforms.Android
+namespace BookstoreApp
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
--- BookstoreApp/Platforms/Android/MainApplication.cs
+++ BookstoreApp/Platforms/Android/MainApplication.cs
@@ -1,12 +1,12 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-namespace BookstoreApp.Platforms.Android
+namespace BookstoreApp
 {
     [Application]
     public class MainApplication : MauiApplication
     {
-        public MainApplication(nint handle, JniHandleOwnership ownership)
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
             : base(handle, ownership)
         {
         }
