931002e192fa57af1ecb90f6e7b4daf626585989
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-15 20:58:51 +0200

    Added CRUD operation to API with HttpClient. Fixed minor spelling mistake of AddressViewModel

---
+++ BookstoreApp/MVVM/ViewModels/AddressViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore_MAUI.MVVM.ViewModels
+{
+    public partial class AddressViewModel : ObservableObject
+    {
+        private readonly HttpClient _httpClient;
+        private const string BaseUrl = "https://localhost:7299/Address";
+
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string AddInfo { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Province { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public bool IsPrimary { get; set; }
+        public List<ClientViewModel> Clients { get; set; }
+
+        public AddressViewModel (HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<AddressViewModel>> GetAllAddressesAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<AddressViewModel>>($"{BaseUrl}/all");
+        }
+
+        public async Task<AddressViewModel> GetAddressByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<AddressViewModel>($"{BaseUrl}/{id}");
+        }
+
+        public async Task<bool> UpdateAddressAsync(AddressViewModel address)
+        {
+            if (address == null)
+            {
+                return false;
+            }
+            var response = await _httpClient.PutAsJsonAsync($"{BaseUrl}/{address.Id}", address);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
--- BookstoreApp/MVVM/ViewModels/AuthorViewModel.cs
+++ BookstoreApp/MVVM/ViewModels/AuthorViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,48 @@ namespace Bookstore_MAUI.MVVM.ViewModels
 {
     public partial class AuthorViewModel : ObservableObject
     {
-        //private readonly ApplicationDbContext _dbContext;
+        private readonly HttpClient _httpClient;
+        private const string BaseUrl = "https://localhost:7299/Author";
 
         public int Id { get; set; }
         public string Name { get; set; }
         public List<BookViewModel> Books { get; set; }
 
+        public AuthorViewModel (HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
 
+        public async Task<IEnumerable<AuthorViewModel>> GetAllAuthorsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<AuthorViewModel>>($"{BaseUrl}/all");
+        }
+
+        public async Task<AuthorViewModel> GetAuthorByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<AuthorViewModel>($"{BaseUrl}/{id}");
+        }
+
+        public async Task<bool>AddAuthorAsync(AuthorViewModel author)
+        {
+            var response = await _httpClient.PostAsJsonAsync($"{BaseUrl}/add", author);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> UpdateAuthorAsync(AuthorViewModel author)
+        {
+            if (author == null)
+            {
+                return false;
+            }
+            var response = await _httpClient.PutAsJsonAsync($"{BaseUrl}/{author.Id}", author);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteAuthorAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{BaseUrl}/{id}");
+            return response.IsSuccessStatusCode;
+        }
     }
 }
