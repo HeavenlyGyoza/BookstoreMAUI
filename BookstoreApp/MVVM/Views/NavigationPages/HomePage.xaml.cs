f63c34274cbe7fa03bdf2917cf47619f3fcd0094
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-16 21:05:08 +0200

    Added search bar

--- BookstoreApp/MVVM/Views/NavigationPages/HomePage.xaml.cs
+++ BookstoreApp/MVVM/Views/NavigationPages/HomePage.xaml.cs
@@ -1,3 +1,5 @@
+using BookstoreApp.MVVM.Views.ApplicationPages;
+
 namespace BookstoreApp.MVVM.Views.NavigationPages;
 
 public partial class HomePage : ContentPage
@@ -6,4 +8,27 @@ public partial class HomePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void SearchButton_Clicked(object sender, EventArgs e)
+    {
+		if (SearchBar.IsVisible)
+		{
+			await SearchBar.TranslateTo(0, SearchBar.Height, 250);
+			//await SearchBar.FadeTo(0, 250);
+			SearchBar.IsVisible = false;
+		}
+		else
+		{
+			SearchBar.IsVisible = true;
+			await SearchBar.TranslateTo(0, 0, 250);
+			SearchBar.Focus();
+			//await SearchBar.FadeTo(1, 250);
+		}
+    }
+
+    private async void SearchItems(object sender, EventArgs e)
+    {
+		string query = ((SearchBar)sender).Text;
+		await Shell.Current.GoToAsync($"{nameof(ExplorePage)}?query={Uri.EscapeDataString(query)}");
+    }
 }
