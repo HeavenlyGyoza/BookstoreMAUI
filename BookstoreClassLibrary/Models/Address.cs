5a99ad3f8253302960269716d522ad45dd722156
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-17 19:00:03 +0200

    Created Class Library and added models

---
+++ BookstoreClassLibrary/Models/Address.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreClassLibrary.Models
+{
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Street { get; set; }
+        public string AddInfo { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        [MaxLength(5)]
+        public string PostalCode { get; set; }
+        [Required]
+        public string Province { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        [MaxLength(3)]
+        public string Country { get; set; }
+        [Required]
+        public bool IsPrimary { get; set; }
+        public ICollection<Client> Clients { get; set; }
+    }
+}
---
+++ BookstoreClassLibrary/Models/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreClassLibrary.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Surname { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+        public ICollection<Address> Adresses { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
