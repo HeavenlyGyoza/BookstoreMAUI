70617f94692a90b00feb52746e140cd49e25e1e2
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-18 18:55:50 +0200

    Added a Search Service to provide separation of logic

---
+++ BookstoreApp/MVVM/Services/SearchService.cs
@@ -0,0 +1,49 @@
+﻿using BookstoreClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreApp.MVVM.Services
+{
+    public class SearchService
+    {
+        private readonly HttpClient _httpClient;
+        private const string BaseUrl = "https://localhost:7299";
+
+        public SearchService (HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Author>> SearchAuthorsAsync(string query)
+        {
+            var authors = await _httpClient.GetFromJsonAsync<IEnumerable<Author>>($"{BaseUrl}/Author/byName?query={query}");
+
+            return authors;
+        }
+
+        public async Task<IEnumerable<Book>> SearchBooksAsync(string query)
+        {
+            var books = new List<Book>();
+
+            var authors = await SearchAuthorsAsync(query);
+            if (authors != null)
+            {
+                foreach (var author in authors)
+                {
+                    books.AddRange(author.Books);
+                }
+            }
+
+            var booksByTitle = await _httpClient.GetFromJsonAsync<IEnumerable<Book>>($"{BaseUrl}/Book/byTitle?query={query}");
+            if (booksByTitle != null)
+            {
+                books.AddRange(booksByTitle.Where(b => !books.Contains(b)));
+            }
+            return books;
+        }
+    }
+}
