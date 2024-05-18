b6162b6b2f97afa5c0f03bb2381d16270e847c27
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-18 21:24:09 +0200

    Added new shell routings

--- BookstoreApp/MauiProgram.cs
+++ BookstoreApp/MauiProgram.cs
@@ -1,7 +1,9 @@
 ﻿using Bookstore_MAUI.MVVM.ViewModels;
 using BookstoreApp.MVVM.Services;
+using BookstoreApp.MVVM.Views.AdminPages;
 using BookstoreApp.MVVM.Views.ApplicationPages;
 using BookstoreApp.MVVM.Views.LoginPages;
+using BookstoreApp.MVVM.Views.NavigationPages;
 using Microsoft.Extensions.Logging;
 
 namespace BookstoreApp
@@ -32,6 +34,8 @@ namespace BookstoreApp
             //Register Shell routes
             Routing.RegisterRoute(nameof(RegistrationPage), typeof(RegistrationPage));
             Routing.RegisterRoute(nameof(ExplorePage), typeof(ExplorePage));
+            Routing.RegisterRoute($"{nameof(ManagementPage)}/{nameof(AdminStockPage)}", typeof(AdminStockPage));
+            Routing.RegisterRoute($"{nameof(ManagementPage)}/{nameof(AdminStockPage)}/{nameof(AddBookPage)}", typeof(AddBookPage));
 
 #if DEBUG
     		builder.Logging.AddDebug();
