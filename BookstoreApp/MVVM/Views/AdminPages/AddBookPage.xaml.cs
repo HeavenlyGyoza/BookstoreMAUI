db56266a794ca882985dd3627f233200f8bd6c77
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-21 17:34:06 +0200

    Added FilePicker to upload images

--- BookstoreApp/MVVM/Views/AdminPages/AddBookPage.xaml.cs
+++ BookstoreApp/MVVM/Views/AdminPages/AddBookPage.xaml.cs
@@ -6,6 +6,7 @@ namespace BookstoreApp.MVVM.Views.AdminPages;
 public partial class AddBookPage : ContentPage
 {
 	private readonly BookViewModel _bookVM;
+	private MemoryStream _bookCoverStream;
 	public AddBookPage(BookViewModel bookViewModel)
 	{
 		InitializeComponent();
@@ -24,20 +25,38 @@ public partial class AddBookPage : ContentPage
 			Language = languageEntry.Text,
 			PageSize = int.Parse(pageSizeEntry.Text),
 			Description = descriptionEditor.Text,
-			CoverImage = coverImageEntry.Text,
 			Stock = int.Parse(stockEntry.Text),
 			Price = decimal.Parse(priceEntry.Text),
 			Authors = authorsEntry.Text.Split(',').Select(name => new Author { Name = name.Trim() }).ToList()
 		};
 
-		bool success = await _bookVM.AddBookAsync(book);
+		bool success = await _bookVM.AddBookAsync(book, _bookCoverStream);
 		if (success)
 		{
 			await DisplayAlert("Sucess", "Book added succesfully!", "OK");
+			await Shell.Current.GoToAsync("..");
 		}
 		else
 		{
 			await DisplayAlert("Error", "Failed to add book.", "OK");
+		}
+    }
+
+    private async void FilePickerButtonClicked(object sender, EventArgs e)
+    {
+		var result = await FilePicker.PickAsync(new PickOptions
+		{
+			FileTypes = FilePickerFileType.Images,
+			PickerTitle = "Please select an image"
+		});
+
+		if (result != null)
+		{
+			bookCoverImage.Source = ImageSource.FromFile(result.FullPath);
+			var stream = await result.OpenReadAsync();
+			_bookCoverStream = new MemoryStream();
+			await stream.CopyToAsync(_bookCoverStream);
+			_bookCoverStream.Position = 0;
 		}
     }
 }
