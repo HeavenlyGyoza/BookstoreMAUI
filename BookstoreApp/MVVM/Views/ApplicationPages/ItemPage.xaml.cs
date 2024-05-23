2732996822ff8f5119350bd5278f0b169e2b14fa
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-23 17:30:50 +0200

    Added UI

--- BookstoreApp/MVVM/Views/ApplicationPages/ItemPage.xaml.cs
+++ BookstoreApp/MVVM/Views/ApplicationPages/ItemPage.xaml.cs
@@ -1,9 +1,14 @@
+using Bookstore_MAUI.MVVM.ViewModels;
+
 namespace BookstoreApp.MVVM.Views.ApplicationPages;
 
+[QueryProperty(nameof(Id), nameof(Id))]
 public partial class ItemPage : ContentPage
 {
-	public ItemPage()
+    private readonly BookViewModel _bookVM;
+    public ItemPage(BookViewModel bookViewModel)
 	{
 		InitializeComponent();
-	}
+		BindingContext = _bookVM = bookViewModel;
+    }
 }
