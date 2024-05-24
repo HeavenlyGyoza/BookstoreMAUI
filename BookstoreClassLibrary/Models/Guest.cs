6ee4beb715f984819d9890e7965aae6c6bad07ff
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-24 20:42:48 +0200

    Created Guest, inherit from Client

---
+++ BookstoreClassLibrary/Models/Guest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreClassLibrary.Models
+{
+    public class Guest : Client
+    {
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Phone]
+        public string? Phone { get; set; }
+    }
+}
