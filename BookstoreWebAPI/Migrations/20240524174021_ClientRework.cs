d074952fa5eea17acf446a2a60157ada5a0e1563
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-24 20:42:56 +0200

    Added migrations

---
+++ BookstoreWebAPI/Migrations/20240524174021_ClientRework.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookstoreWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClientRework : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Clients",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Clients",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Books",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "DiscountPercentage",
+                table: "Books",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShoppingCartId",
+                table: "Books",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WishlistId",
+                table: "Books",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ShoppingCarts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCarts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShoppingCarts_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wishlists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    WishlistName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wishlists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Wishlists_Clients_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_ShoppingCartId",
+                table: "Books",
+                column: "ShoppingCartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_WishlistId",
+                table: "Books",
+                column: "WishlistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCarts_ClientId",
+                table: "ShoppingCarts",
+                column: "ClientId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wishlists_UserId",
+                table: "Wishlists",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_ShoppingCarts_ShoppingCartId",
+                table: "Books",
+                column: "ShoppingCartId",
+                principalTable: "ShoppingCarts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Wishlists_WishlistId",
+                table: "Books",
+                column: "WishlistId",
+                principalTable: "Wishlists",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_ShoppingCarts_ShoppingCartId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Wishlists_WishlistId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "ShoppingCarts");
+
+            migrationBuilder.DropTable(
+                name: "Wishlists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_ShoppingCartId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_WishlistId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountPercentage",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "ShoppingCartId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "WishlistId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
