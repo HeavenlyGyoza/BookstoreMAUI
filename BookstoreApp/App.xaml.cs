7a2cd2b208f3f3bb0f729b931dda087735dcfdb5
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-25 22:39:32 +0200

    Added check on start of user being logged in

--- BookstoreApp/App.xaml.cs
+++ BookstoreApp/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace BookstoreApp
+﻿
+using BookstoreApp.MVVM.Views.LoginPages;
+
+namespace BookstoreApp
 {
     public partial class App : Application
     {
@@ -7,6 +10,21 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+        }
+
+        protected override async void OnStart()
+        {
+            base.OnStart();
+            if (!await IsUserLoggedIn())
+            {
+                await Shell.Current.GoToAsync(nameof(LoginPage));
+            }
+        }
+
+        private async Task<bool> IsUserLoggedIn()
+        {
+            var isLogged = await SecureStorage.GetAsync("IsLoggedIn");
+            return isLogged != null && isLogged == "true";
         }
     }
 }
