5d4bc464624143260da32ed7c3745a90753a1c67
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-25 22:36:51 +0200

    Added commands and LoginAsync method

--- BookstoreApp/MVVM/ViewModels/UserViewModel.cs
+++ BookstoreApp/MVVM/ViewModels/UserViewModel.cs
@@ -1,5 +1,8 @@
-﻿using BookstoreClassLibrary.Models;
+﻿using BookstoreApp.MVVM.Views.LoginPages;
+using BookstoreApp.MVVM.Views.NavigationPages;
+using BookstoreClassLibrary.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,9 +25,16 @@ namespace Bookstore_MAUI.MVVM.ViewModels
         [ObservableProperty]
         public string role;
 
+        public IRelayCommand LoginCommand { get; }
+        public IRelayCommand ToRegisterPageCommand { get; }
+        public IRelayCommand SignUpCommand { get; }
+
         public UserViewModel (HttpClient httpClient)
         {
             _httpClient = httpClient;
+            LoginCommand = new RelayCommand<User>(LoginCommandAction);
+            ToRegisterPageCommand = new RelayCommand(ToRegisterPageCommandAction);
+            SignUpCommand = new RelayCommand<User>(SignUpCommandAction);
         }
 
         public async Task<IEnumerable<User>> GetAllUsersAsync()
@@ -35,6 +45,21 @@ namespace Bookstore_MAUI.MVVM.ViewModels
         public async Task<User> GetUserByIdAsync(int id)
         {
             return await _httpClient.GetFromJsonAsync<User>($"{BaseUrl}/{id}");
+        }
+
+        public async Task<User> LoginAsync(string email, string password)
+        {
+            var user = new User { Email = email, Password = password };
+            var response = await _httpClient.PostAsJsonAsync($"{BaseUrl}/login", user);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<User>();
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Login failed", "Invalid username or password.", "OK");
+            }
+            return null;
         }
 
         public async Task<bool> AddUserAsync(User user)
@@ -53,6 +78,41 @@ namespace Bookstore_MAUI.MVVM.ViewModels
         {
             var response = await _httpClient.DeleteAsync($"{BaseUrl}/{id}");
             return response.IsSuccessStatusCode;
+        }
+
+        public async void LoginCommandAction(User user)
+        {
+            if (user != null)
+            {
+                var response = LoginAsync(user.Email, user.Password);
+                if (response != null)
+                {
+                    await SecureStorage.SetAsync("IsLoggedIn", "true");
+                    await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+                }
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Login failed", "Invalid username or password.", "OK");
+            }
+        }
+
+        public async void ToRegisterPageCommandAction()
+        {
+            await Shell.Current.GoToAsync($"{nameof(RegistrationPage)}");
+        }
+
+        public async void SignUpCommandAction(User user)
+        {
+            if (user != null)
+            {
+                var response = await AddUserAsync(user);
+                await Shell.Current.GoToAsync($"..");
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Invalid username or password.", "OK");
+            }
         }
     }
 }
