0b3dc6f84a160a2c1d65b1414eb657fd4cfcaa28
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-27 22:00:55 +0200

    Recover and set logged in client on app restart

--- BookstoreApp/App.xaml.cs
+++ BookstoreApp/App.xaml.cs
@@ -1,21 +1,39 @@
 ﻿
+using Bookstore_MAUI.MVVM.ViewModels;
 using BookstoreApp.MVVM.Views.LoginPages;
 
 namespace BookstoreApp
 {
     public partial class App : Application
     {
-        public App()
+        private readonly ClientViewModel _clientViewModel;
+        public App(ClientViewModel clientViewModel)
         {
             InitializeComponent();
-
+            _clientViewModel = clientViewModel;
             MainPage = new AppShell();
         }
 
         protected override async void OnStart()
         {
             base.OnStart();
-            if (!await IsUserLoggedIn())
+            if (await IsUserLoggedIn())
+            {
+                var clientId = await SecureStorage.GetAsync("ClientId");
+                if (clientId != null)
+                {                    
+                    var client = await _clientViewModel.GetClientByIdAsync(int.Parse(clientId));
+                    _clientViewModel.Id = client.Id;
+                    _clientViewModel.Name = client.Name;
+                    _clientViewModel.Surname = client.Surname;
+                    _clientViewModel.Email = client.Email;
+                    _clientViewModel.Password = client.Password;
+                    _clientViewModel.Role = client.Role;
+                    _clientViewModel.Phone = client.Phone;
+                    _clientViewModel.Addresses = client.Addresses.ToList();
+                }
+            }
+            else
             {
                 await Shell.Current.GoToAsync(nameof(LoginPage));
             }
