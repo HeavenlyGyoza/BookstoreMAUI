ca3da7d7d1840715c9f4e025bce386c3d0328617
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-05-27 20:06:11 +0200

    Bindings changed from User to client viewmodels

--- BookstoreApp/MVVM/Views/LoginPages/LoginPage.xaml.cs
+++ BookstoreApp/MVVM/Views/LoginPages/LoginPage.xaml.cs
@@ -5,17 +5,17 @@ namespace BookstoreApp.MVVM.Views.LoginPages;
 
 public partial class LoginPage : ContentPage
 {
-    private readonly UserViewModel _userVM;
-	public LoginPage(UserViewModel userViewModel)
+    private readonly ClientViewModel _clientVM;
+	public LoginPage(ClientViewModel clientViewModel)
 	{
 		InitializeComponent();
-        BindingContext = _userVM = userViewModel;
+        BindingContext = _clientVM = clientViewModel;
 	}
 
     private async void LoginButtonClicked(object sender, EventArgs e)
     {
-        var user = await _userVM.LoginAsync(_userVM.Email, _userVM.Password);
-        if (user != null)
+        var client = await _clientVM.LoginAsync(_clientVM.Email, _clientVM.Password);
+        if (client != null)
         {
             await SecureStorage.SetAsync("IsLoggedIn", "true");
             await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
