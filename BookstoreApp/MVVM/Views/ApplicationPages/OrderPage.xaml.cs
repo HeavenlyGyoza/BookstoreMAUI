45fad4f08a83cb33a37e7c3c43d27d21aec25e3d
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-06-05 22:42:25 +0200

    Added retrieval of logged in clientt

--- BookstoreApp/MVVM/Views/ApplicationPages/OrderPage.xaml.cs
+++ BookstoreApp/MVVM/Views/ApplicationPages/OrderPage.xaml.cs
@@ -1,13 +1,35 @@
 using Bookstore_MAUI.MVVM.ViewModels;
+using BookstoreClassLibrary.Models;
 
 namespace BookstoreApp.MVVM.Views.ApplicationPages;
 
 public partial class OrderPage : ContentPage
 {
-    private readonly BookViewModel _bookVM;
-    public OrderPage(BookViewModel bookVM)
+    private readonly OrderViewModel _orderVM;
+    private readonly ClientViewModel _clientVM;
+    public OrderPage(OrderViewModel orderVM, ClientViewModel clientVM)
     {
         InitializeComponent();
-        BindingContext = _bookVM = bookVM;
+        BindingContext = _orderVM = orderVM;
+        _clientVM = clientVM;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (await IsUserLoggedIn())
+        {
+            var clientId = await SecureStorage.GetAsync("ClientId");
+            if (clientId != null)
+            {
+                _orderVM.Client = await _clientVM.GetClientByIdAsync(int.Parse(clientId));
+            }
+        }
+    }
+
+    private async Task<bool> IsUserLoggedIn()
+    {
+        var isLogged = await SecureStorage.GetAsync("IsLoggedIn");
+        return isLogged != null && isLogged == "true";
     }
 }
