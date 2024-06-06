e0fb73e5586df5a5a8b4c557f6218636871abcdc
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-06-07 01:38:26 +0200

    Added CollectionView for order history

--- BookstoreApp/MVVM/Views/NavigationPages/UserPage.xaml.cs
+++ BookstoreApp/MVVM/Views/NavigationPages/UserPage.xaml.cs
@@ -5,10 +5,12 @@ namespace BookstoreApp.MVVM.Views.NavigationPages;
 public partial class UserPage : ContentPage
 {
     private readonly ClientViewModel _clientVM;
-    public UserPage(ClientViewModel clientViewModel)
+    private readonly OrderViewModel _orderVM;
+    public UserPage(ClientViewModel clientViewModel, OrderViewModel orderVM)
 	{
 		InitializeComponent();
         BindingContext = _clientVM = clientViewModel;
+        _orderVM = orderVM;
     }
 
     protected override void OnAppearing()
@@ -18,6 +20,11 @@ public partial class UserPage : ContentPage
         {
             signInButton.IsVisible = false;
             userGrid.IsVisible = true;
+        }
+        if (_orderVM != null && _orderVM.LoadClientOrdersCommand.CanExecute(null))
+        {
+            BindingContext = _orderVM; //necessary to show orders collection
+            _orderVM.LoadClientOrdersCommand.Execute(null);
         }
     }
 
