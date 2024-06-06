859a82a8750046e24181439630776a3ab9abe005
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-06-06 23:34:27 +0200

    Added foreign keys for EF

--- BookstoreClassLibrary/Models/Order.cs
+++ BookstoreClassLibrary/Models/Order.cs
@@ -20,11 +20,17 @@ namespace BookstoreClassLibrary.Models
         public decimal Price { get; set; }
         [Required]
         public DateOnly OrderDate { get; set; }
+        [ForeignKey(nameof(Client))]
         [Required]
+        public int ClientId { get; set; }
         public Client Client { get; set; }
+        [ForeignKey(nameof(Book))]
         [Required]
+        public int BookId { get; set; }
         public Book Book { get; set; }
+        [ForeignKey(nameof(Address))]
         [Required]
+        public int AddressId { get; set; }
         public Address Address { get; set; }
     }
 }
