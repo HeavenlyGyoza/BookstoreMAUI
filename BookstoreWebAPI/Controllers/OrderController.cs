56bc8b07f160933383bb533600e2d77e44440ce0
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-06-06 23:35:08 +0200

    Improved AddOrder logic, added logger

--- BookstoreWebAPI/Controllers/OrderController.cs
+++ BookstoreWebAPI/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using BookstoreWebAPI.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace BookstoreWebAPI.Controllers
 {
@@ -10,9 +11,11 @@ namespace BookstoreWebAPI.Controllers
     public class OrderController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
-        public OrderController(ApplicationDbContext dbContext)
+        private readonly ILogger<OrderController> _logger;
+        public OrderController(ApplicationDbContext dbContext, ILogger<OrderController> logger)
         {
             _dbContext = dbContext;
+            _logger = logger;
         }
 
         [HttpGet("all")]
@@ -37,12 +40,65 @@ namespace BookstoreWebAPI.Controllers
         {
             if (order == null)
             {
-                return BadRequest();
+                _logger.LogError("Order is null.");
+                return BadRequest("Order is null.");
             }
-            _dbContext.Orders.Add(order);
-            _dbContext.SaveChangesAsync();
-            return Ok();
+
+            try
+            {
+                if (order.ClientId > 0)
+                {
+                    var client = await _dbContext.Clients.FindAsync(order.ClientId);
+                    if (client == null)
+                    {
+                        return NotFound("Client not found");
+                    }
+                    order.Client = client;
+                }
+
+                if (order.BookId > 0)
+                {
+                    var book = await _dbContext.Books.FindAsync(order.BookId);
+                    if (book == null)
+                    {
+                        return NotFound("Book not found");
+                    }
+                    order.Book = book;
+                    book.Stock -= order.Quantity;
+                    _dbContext.Entry(book).State = EntityState.Modified;
+                }
+
+                var existingAddress = await _dbContext.Addresses.FirstOrDefaultAsync(a =>
+                                                                a.Street == order.Address.Street &&
+                                                                a.City == order.Address.City &&
+                                                                a.State == order.Address.State &&
+                                                                a.PostalCode == order.Address.PostalCode);
+
+                if (existingAddress != null)
+                {
+                    order.AddressId = existingAddress.Id;
+                    order.Address = existingAddress;
+                }
+                else
+                {
+                    _dbContext.Addresses.Add(order.Address);
+                }
+
+
+                _logger.LogInformation($"Order details: {JsonSerializer.Serialize(order)}");
+
+                _dbContext.Orders.Add(order);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(order);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception: {ex.Message}");
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
         }
+
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateOrder(int id, Order order)
         {
