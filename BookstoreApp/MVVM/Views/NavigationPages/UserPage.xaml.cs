63ddab68b794d3bd47a39c9ec48cfa438f49c9d5
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-06-07 22:07:24 +0200

    Fixed language radio group, grid visibility

--- BookstoreApp/MVVM/Views/NavigationPages/UserPage.xaml.cs
+++ BookstoreApp/MVVM/Views/NavigationPages/UserPage.xaml.cs
@@ -8,48 +8,55 @@ namespace BookstoreApp.MVVM.Views.NavigationPages;
 public partial class UserPage : ContentPage
 {
     private readonly ClientViewModel _clientVM;
-    private readonly OrderViewModel _orderVM;
-    public LocalizationResourceManager LocalizationResourceManager => LocalizationResourceManager.Instance;
-    public UserPage(ClientViewModel clientViewModel, OrderViewModel orderVM)
+    private bool _IsInitialized = false;
+    public UserPage(ClientViewModel clientViewModel)
 	{
 		InitializeComponent();
         BindingContext = _clientVM = clientViewModel;
-        _orderVM = orderVM;
+        _IsInitialized = true;
+        SetLanguageRG();
     }
 
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        if (_clientVM != null)
+        if (_clientVM.Id > 0)
         {
             signInButton.IsVisible = false;
             userGrid.IsVisible = true;
-        }
-        if (_orderVM != null && _orderVM.LoadClientOrdersCommand.CanExecute(null))
-        {
-            BindingContext = _orderVM; //necessary to show orders collection
-            _orderVM.LoadClientOrdersCommand.Execute(null);
         }
     }
 
     private void rbEnglish_CheckedChanged(object sender, CheckedChangedEventArgs e)
     {
-        if (e.Value)
+        if (_IsInitialized && e.Value)
         {
             var switchToCulture = new CultureInfo("en-US");
-            BindingContext = this;
-            LocalizationResourceManager.SetCulture(switchToCulture);
+            LocalizationResourceManager.Instance.SetCulture(switchToCulture);
+            SecureStorage.SetAsync("AppLanguage", switchToCulture.Name);
         }
 
     }
 
     private void rbSpanish_CheckedChanged(object sender, CheckedChangedEventArgs e)
     {
-        if (e.Value)
+        if (_IsInitialized && e.Value)
         {
             var switchToCulture = new CultureInfo("es-ES");
-            BindingContext = this;
-            LocalizationResourceManager.SetCulture(switchToCulture);
+            LocalizationResourceManager.Instance.SetCulture(switchToCulture);
+            SecureStorage.SetAsync("AppLanguage", switchToCulture.Name);
+        }
+    }
+
+    private void SetLanguageRG()
+    {
+        if (_clientVM.lang.Name == "en-US")
+        {
+            rbEnglish.IsChecked = true;
+        }
+        else if(_clientVM.lang.Name == "es-ES")
+        {
+            rbSpanish.IsChecked = true;
         }
     }
 }
