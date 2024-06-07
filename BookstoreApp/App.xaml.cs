01c6749f7d276b7a5dd18a46d065468e3625c54e
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-06-07 22:08:48 +0200

    Moved logged user logic to viewmodel. Correclty retrieves saved language preferences

--- BookstoreApp/App.xaml.cs
+++ BookstoreApp/App.xaml.cs
@@ -9,45 +9,29 @@ namespace BookstoreApp
     public partial class App : Application
     {
         private readonly ClientViewModel _clientViewModel;
-        public LocalizationResourceManager LocalizationResourceManager => LocalizationResourceManager.Instance;
         public App(ClientViewModel clientViewModel)
         {
             InitializeComponent();
             _clientViewModel = clientViewModel;
-            var switchToCulture = new CultureInfo("en-US");
-            LocalizationResourceManager.SetCulture(switchToCulture);
+            var savedCulture = SecureStorage.GetAsync("AppLanguage").GetAwaiter().GetResult();
+            var culture = savedCulture != null ? new CultureInfo(savedCulture) : new CultureInfo("en-US");
+            LocalizationResourceManager.Instance.SetCulture(culture);
+            _clientViewModel.lang = culture;
             MainPage = new AppShell();
         }
 
         protected override async void OnStart()
         {
             base.OnStart();
-            if (await IsUserLoggedIn())
+            try
             {
-                var clientId = await SecureStorage.GetAsync("ClientId");
-                if (clientId != null)
-                {                    
-                    var client = await _clientViewModel.GetClientByIdAsync(int.Parse(clientId));
-                    _clientViewModel.Id = client.Id;
-                    _clientViewModel.Name = client.Name;
-                    _clientViewModel.Surname = client.Surname;
-                    _clientViewModel.Email = client.Email;
-                    _clientViewModel.Password = client.Password;
-                    _clientViewModel.Role = client.Role;
-                    _clientViewModel.Phone = client.Phone;
-                    _clientViewModel.Addresses = client.Addresses.ToList();
-                }
+                await _clientViewModel.LoadClientDataAsync();
             }
-            else
+            catch (Exception ex)
             {
+                await Current.MainPage.DisplayAlert("Error", $"Failed to load client data: {ex.Message}", "OK");
                 await Shell.Current.GoToAsync(nameof(LoginPage));
             }
-        }
-
-        private async Task<bool> IsUserLoggedIn()
-        {
-            var isLogged = await SecureStorage.GetAsync("IsLoggedIn");
-            return isLogged != null && isLogged == "true";
         }
     }
 }
