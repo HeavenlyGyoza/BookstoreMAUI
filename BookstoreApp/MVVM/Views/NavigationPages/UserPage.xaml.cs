943381e7398a0ce9a7d1394024c25f1a40941499
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-06-08 18:47:26 +0200

    Moved LogOut button and language radio group to AccountSettings page

--- BookstoreApp/MVVM/Views/NavigationPages/UserPage.xaml.cs
+++ BookstoreApp/MVVM/Views/NavigationPages/UserPage.xaml.cs
@@ -1,20 +1,14 @@
 using Bookstore_MAUI.MVVM.ViewModels;
-using BookstoreApp.MVVM.Services.Localization;
-using BookstoreApp.Resources.Languages;
-using System.Globalization;
 
 namespace BookstoreApp.MVVM.Views.NavigationPages;
 
 public partial class UserPage : ContentPage
 {
     private readonly ClientViewModel _clientVM;
-    private bool _IsInitialized = false;
     public UserPage(ClientViewModel clientViewModel)
 	{
 		InitializeComponent();
         BindingContext = _clientVM = clientViewModel;
-        _IsInitialized = true;
-        SetLanguageRG();
     }
 
     protected override void OnAppearing()
@@ -24,39 +18,6 @@ public partial class UserPage : ContentPage
         {
             signInButton.IsVisible = false;
             userGrid.IsVisible = true;
-        }
-    }
-
-    private void rbEnglish_CheckedChanged(object sender, CheckedChangedEventArgs e)
-    {
-        if (_IsInitialized && e.Value)
-        {
-            var switchToCulture = new CultureInfo("en-US");
-            LocalizationResourceManager.Instance.SetCulture(switchToCulture);
-            SecureStorage.SetAsync("AppLanguage", switchToCulture.Name);
-        }
-
-    }
-
-    private void rbSpanish_CheckedChanged(object sender, CheckedChangedEventArgs e)
-    {
-        if (_IsInitialized && e.Value)
-        {
-            var switchToCulture = new CultureInfo("es-ES");
-            LocalizationResourceManager.Instance.SetCulture(switchToCulture);
-            SecureStorage.SetAsync("AppLanguage", switchToCulture.Name);
-        }
-    }
-
-    private void SetLanguageRG()
-    {
-        if (_clientVM.lang.Name == "en-US")
-        {
-            rbEnglish.IsChecked = true;
-        }
-        else if(_clientVM.lang.Name == "es-ES")
-        {
-            rbSpanish.IsChecked = true;
         }
     }
 }
--- BookstoreApp/MVVM/Views/UserPages/AccountSettingsPage.xaml.cs
+++ BookstoreApp/MVVM/Views/UserPages/AccountSettingsPage.xaml.cs
@@ -1,9 +1,50 @@
+using Bookstore_MAUI.MVVM.ViewModels;
+using BookstoreApp.MVVM.Services.Localization;
+using System.Globalization;
+
 namespace BookstoreApp.MVVM.Views.UserPages;
 
 public partial class AccountSettingsPage : ContentPage
 {
-	public AccountSettingsPage()
+    private readonly ClientViewModel _clientVM;
+    private bool _IsInitialized = false;
+    public AccountSettingsPage(ClientViewModel clientVM)
 	{
 		InitializeComponent();
-	}
+		BindingContext = _clientVM = clientVM;
+        _IsInitialized = true;
+        SetLanguageRG();
+    }
+    private void rbEnglish_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (_IsInitialized && e.Value)
+        {
+            var switchToCulture = new CultureInfo("en-US");
+            LocalizationResourceManager.Instance.SetCulture(switchToCulture);
+            SecureStorage.SetAsync("AppLanguage", switchToCulture.Name);
+        }
+
+    }
+
+    private void rbSpanish_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (_IsInitialized && e.Value)
+        {
+            var switchToCulture = new CultureInfo("es-ES");
+            LocalizationResourceManager.Instance.SetCulture(switchToCulture);
+            SecureStorage.SetAsync("AppLanguage", switchToCulture.Name);
+        }
+    }
+
+    private void SetLanguageRG()
+    {
+        if (_clientVM.lang.Name == "en-US")
+        {
+            rbEnglish.IsChecked = true;
+        }
+        else if (_clientVM.lang.Name == "es-ES")
+        {
+            rbSpanish.IsChecked = true;
+        }
+    }
 }
