4e4896a04066f2819740f3024fe1b498d33da8df
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-06-09 00:28:12 +0200

    Fixed isprimary radio button. Updates address IsPrimary if different address selected as primary

--- BookstoreApp/MVVM/Views/UserPages/UserAddressesPage.xaml.cs
+++ BookstoreApp/MVVM/Views/UserPages/UserAddressesPage.xaml.cs
@@ -1,4 +1,5 @@
 using Bookstore_MAUI.MVVM.ViewModels;
+using BookstoreClassLibrary.Models;
 
 namespace BookstoreApp.MVVM.Views.UserPages;
 
@@ -18,5 +19,23 @@ public partial class UserAddressesPage : ContentPage
         base.OnAppearing();
         await _addressVM.LoadClientAddressesCollection(_clientVM.Id);
 
+    }
+
+    private async void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value)
+        {
+            var selectedAddress = (Address)((RadioButton)sender).BindingContext;
+            foreach (var address in _addressVM.ClientAddresses)
+            {
+                if (address != selectedAddress && address.IsPrimary)
+                {
+                    address.IsPrimary = false;
+                    await _addressVM.UpdateAddressAsync(address);
+                }
+            }
+            selectedAddress.IsPrimary = true;
+            await _addressVM.UpdateAddressAsync(selectedAddress);
+        }
     }
 }
