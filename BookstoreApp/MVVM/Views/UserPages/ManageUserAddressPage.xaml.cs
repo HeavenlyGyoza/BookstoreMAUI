a388da6c70485ef5322c25bb5b073017f43d37e7
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-06-09 13:54:29 +0200

    Fixed adding new address

--- BookstoreApp/MVVM/Views/UserPages/ManageUserAddressPage.xaml.cs
+++ BookstoreApp/MVVM/Views/UserPages/ManageUserAddressPage.xaml.cs
@@ -1,13 +1,32 @@
 using Bookstore_MAUI.MVVM.ViewModels;
+using BookstoreClassLibrary.Models;
 
 namespace BookstoreApp.MVVM.Views.UserPages;
 
 public partial class ManageUserAddressPage : ContentPage
 {
     private readonly AddressViewModel _addressVM;
-    public ManageUserAddressPage(AddressViewModel addressVM)
-	{
-		InitializeComponent();
-		BindingContext = _addressVM = addressVM;
-	}
+	private readonly ClientViewModel _clientVM;
+    public ManageUserAddressPage(AddressViewModel addressVM, ClientViewModel clientVM)
+    {
+        InitializeComponent();
+        BindingContext = _addressVM = addressVM;
+        _clientVM = clientVM;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        Client client = new Client
+        {
+            Id = _clientVM.Id,
+            Name = _clientVM.Name,
+            Surname = _clientVM.Surname,
+            Email = _clientVM.Email,
+            Password = _clientVM.Password,
+            Phone = _clientVM.Phone,
+            Addresses = _clientVM.Addresses
+        };
+        _addressVM.client = client;
+    }
 }
