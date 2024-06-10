f585e88b40dd5999bb483c4196a344faf49e104f
HeavenlyGyoza <s.gfranco@protonmail.com>
2024-06-10 02:43:32 +0200

    Tab page visible again

--- BookstoreApp/AppShell.xaml.cs
+++ BookstoreApp/AppShell.xaml.cs
@@ -4,27 +4,15 @@ namespace BookstoreApp
 {
     public partial class AppShell : Shell
     {
-        private readonly ClientViewModel _clientVM;
-        public AppShell(ClientViewModel clientVM)
+        public AppShell()
         {
             InitializeComponent();
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NBaF1cXmhPYVJ/WmFZfVpgdVdMYl5bRXRPIiBoS35RckVmW39fcnZXR2NYWUB3");
             Application.Current.UserAppTheme = AppTheme.Light;
-            _clientVM = clientVM;
-            CheckUserRole();
         }
 
-        private void CheckUserRole()
-        {
-            if (_clientVM.Role != "admin")
-            {
-                managementTab.IsVisible = false;
-            }
-        }
-
-        //protected override void OnAppearing()
+        //private void CheckUserRole()
         //{
-        //    base.OnAppearing();
         //    if (_clientVM.Role != "admin")
         //    {
         //        managementTab.IsVisible = false;
